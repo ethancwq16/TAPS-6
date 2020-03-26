ea7e2d43bbe2726f9d73abcdb5cb0db9a25df207
ethancwq16 <ethan.cwq96@gmail.com>
2020-03-26 20:51:37 +0800

    Successfully get latlon according to user input and display image

--- javascript/loadImage.js
+++ javascript/loadImage.js
@@ -1,4 +1,28 @@
 function loadImage() {
-  console.log("here");
+  var location = document.getElementById("location").value;
+  // console.log(location);
+
+  var date = document.getElementById("date").value;
+  var time = document.getElementById("time").value;
+  var date_time = date + "T" + time.substring(0,2) + "%3A" +  time.substring(3,5) + "%3A00%2B08%3A00";
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var traffic = JSON.parse(this.responseText);
+      for (var i = 0; i < traffic.items[0].cameras.length; i++) {
+        var trafficLocation = JSON.stringify(traffic.items[0].cameras[i].location)
+        if (trafficLocation == location) {
+          console.log(traffic.items[0].cameras[i].image);
+          //traffic.items[0].cameras[i].image
+          document.getElementById("traffic-img").innerHTML = '<img src="' + traffic.items[0].cameras[i].image + '">';
+
+        }
+      }
+    }
+  }
+
+  xhttp.open("GET", "https://api.data.gov.sg/v1/transport/traffic-images?date_time=" + date_time, true);
+  xhttp.send();
   document.getElementById("traffic-img").innerHTML = '<img src="https://images.data.gov.sg/api/traffic-images/2020/03/95b6827c-ef21-4180-9dee-c09f6d2d1263.jpg">'
 }
--- javascript/loadareas.js
+++ javascript/loadareas.js
@@ -1,10 +1,9 @@
 function loadAreas() {
   var date = document.getElementById("date").value;
   var time = document.getElementById("time").value;
-  console.log(date);
-  console.log(time);
-  var date_time = date + "T" + time + "+8:00";
-  console.log(date_time);
+  var date_time = date + "T" + time.substring(0,2) + "%3A" +  time.substring(3,5) + "%3A00%2B08%3A00";
+
+  // console.log(date_time);
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -19,15 +18,15 @@ function loadAreas() {
         // console.log(latlon);
       }
 
-      var dom = document.getElementById("location");
+      // display areas for selection
+      var doc = document.getElementById("location");
       for (var j = 0; j < latlon.length; j++) {
-        dom.innerHTML = dom.innerHTML + '<option value="">' + JSON.stringify(latlon[j]) + '</option>';
+        var option = JSON.stringify(latlon[j]);
+        doc.innerHTML = doc.innerHTML + "<option value=" + option + ">" + option + "</option>";
       }
     }
   }
-  
-  // date-time format: 2020-02-26T01:35:49+08:00
-  xhttp.open("GET", "https://api.data.gov.sg/v1/transport/traffic-images?date_time=2020-02-26T01%3A35%3A49%2B08%3A00", true);
-  //xhttp.open("GET", "https://api.data.gov.sg/v1/transport/traffic-images?date_time=" + date_time, true);
+
+  xhttp.open("GET", "https://api.data.gov.sg/v1/transport/traffic-images?date_time=" + date_time, true);
   xhttp.send();
 }
